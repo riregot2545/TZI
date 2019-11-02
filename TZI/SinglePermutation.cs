944f363ba4ceb8af8d3f326ec428fdd9ecf34fa6
FusionMike99 <fusionmike99@gmail.com>
2019-11-02 10:32:14 +0200

    added to new class set functions

--- TZI/SinglePermutation.cs
+++ TZI/SinglePermutation.cs
@@ -8,5 +8,31 @@ namespace TZI
 {
     class SinglePermutation
     {
+        private int[] key = null;
+
+        public void setKey(int[] _key)
+        {
+            key = new int[_key.Length];
+            for(int i = 0; i < _key.Length; i++)
+            {
+                key[i] = _key[i];
+            }
+        }
+
+        public void setKey(string[] _key)
+        {
+            key = new int[_key.Length];
+            for(int i = 0; i< _key.Length; i++)
+            {
+                key[i] = Convert.ToInt32(_key[i]);
+            }
+        }
+
+        public void setKey(string _key)
+        {
+            setKey(_key.Split(' '));
+        }
+
+
     }
 }
